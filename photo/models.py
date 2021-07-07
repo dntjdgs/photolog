801ab55946098d5c4d80387ce6e35ef0a4339565
dntjdgs <dntjdgs@gmail.com>
2021-07-07 15:33:32 +0900

    테마적용

--- photo/models.py
+++ photo/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+
+class picture(models.Model):
+    img = models.ImageField(blank=True, upload_to="media")
--- photo/views.py
+++ photo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, "index.html")
+def main(request):
+    return render(request, "index.html", {})
